02b130401ea581db0318c3a1ac91a5a798b6011e
ofirasulin3 <ofirasulin3@gmail.com>
2022-01-03 22:37:52 +0200

    Focal button works

--- app/src/main/java/com/example/networisk/MainActivity.java
+++ app/src/main/java/com/example/networisk/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.IntentFilter;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.wifi.WifiManager;
 import android.os.Build;
@@ -15,10 +17,13 @@ import androidx.core.app.ActivityCompat;;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ListView wifiList;
@@ -27,17 +32,68 @@ public class MainActivity extends AppCompatActivity {
     private final int WiFiPanel = 1;
     private final int LocationPanel = 2;
     WifiReceiver receiverWifi;
+
+    LocationManager locationManager;
+    private Location focalLocation;
+    private double focalLatitude;
+    private double focalLongitude;
+    private Location lastLocation;
+    private double lastLatitude;
+    private double lastLongitude;
+    LocationListener locationListener = new MyLocationListener();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         wifiList = (ListView) findViewById(R.id.wifiList);
-        Button buttonScan = (Button) findViewById(R.id.scanBtn);
+        Button ScanBtn = (Button) findViewById(R.id.scanBtn);
+        Button LocationBtn = (Button) findViewById(R.id.locationBtn);
+
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_ACCESS_FINE_LOCATION);
         }
-        buttonScan.setOnClickListener(new View.OnClickListener() {
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        //5000ms = 5sec-> minimum time for new updates
+        //3m-> minimum distance for new updates
+//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+//                5000, 3, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,0, locationListener);
+
+//        String provider = locationManager.getBestProvider(criteria, true);
+//        Location location = locationManager.getLastKnownLocation(provider);
+
+
+        LocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Permission should be granted at this point
+                // Checking if Location is Enabled
+                if (!isLocationEnabled()){
+                    turnOnLocation();
+                } else{
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_ACCESS_FINE_LOCATION);
+                    }
+                    //TODO: maybe should put here "else"
+//                    Toast.makeText(MainActivity.this, "Getting Focal Location", Toast.LENGTH_SHORT).show();
+//                    Location last_loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//                    Location last_loc = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                    Location last_loc = getLastKnownLocationAux();
+
+                    if(last_loc!=null) focalLatitude = last_loc.getLatitude();
+                    if(last_loc!=null) focalLongitude = last_loc.getLongitude();
+                    Toast.makeText(MainActivity.this,
+                               "New Focal Location: \n" +
+                                    "Lat- " + focalLatitude + "\n" +
+                                    "Lng- " + focalLongitude, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        ScanBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Permission should be granted at this point
@@ -57,8 +113,61 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    //What do on when a screen closes
-    @Override
+
+    private class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location loc) {
+//        editLocation.setText("");
+//        pb.setVisibility(View.INVISIBLE);
+            lastLocation = loc;
+            lastLatitude = loc.getLatitude();
+            lastLongitude = loc.getLongitude();
+            Toast.makeText(MainActivity.this,
+                       "Last Location changed.\n" +
+                            "Lat- " + lastLatitude + "\n" +
+                            "Lng- " + lastLongitude, Toast.LENGTH_LONG).show();
+//            Log.v("Tagl", "Focal Longitude: " + longitude);
+//            Log.v("Tag", "Focal Latitude: " + latitude);
+//            String s = longitude + "\n" + latitude;
+//            editLocation.setText(s);
+        }
+        @Override public void onProviderDisabled(String provider) {}
+        @Override public void onProviderEnabled(String provider) {}
+        @Override public void onStatusChanged(String provider, int status, Bundle extras) {}
+    }
+
+    private Location getLastKnownLocationAux() {
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_ACCESS_FINE_LOCATION);
+        }
+        List<String> providers = locationManager.getProviders(true);
+        Location bestLocation = null;
+        int i = 1;
+        for (String provider : providers) {
+            Location loc = locationManager.getLastKnownLocation(provider);
+//            ALog.d("last known location, provider: %s, location: %s", provider,
+//                    l);
+            if (loc == null) continue;
+            Toast.makeText(MainActivity.this,
+                        "Location Option " + i++ + ":\n" +
+                             "Lat- " + loc.getLatitude() + "\n" +
+                             "Lng- " + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+
+            if (bestLocation == null
+                    || loc.getAccuracy() < bestLocation.getAccuracy()) {
+//                ALog.d("found best last known location: %s", loc);
+                bestLocation = loc;
+            }
+        }
+        if (bestLocation == null) return null;
+        Toast.makeText(MainActivity.this,
+                "Best Known Location:\n" +
+                        "Lat- " + bestLocation.getLatitude() + "\n" +
+                        "Lng- " + bestLocation.getLongitude(), Toast.LENGTH_LONG).show();
+        return bestLocation;
+    }
+
+    @Override //What do on when a screen closes
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(wifiManager.isWifiEnabled()) {
@@ -73,7 +182,7 @@ public class MainActivity extends AppCompatActivity {
                 if (isLocationEnabled()) {
                     Toast.makeText(MainActivity.this, "scanning", Toast.LENGTH_SHORT).show();
                     wifiManager.startScan();
-                } else {//Location is not enabled
+                } else { //Location is not enabled
                     Toast.makeText(getApplicationContext(), "Location is needed for the scan", Toast.LENGTH_LONG).show();
                 }
             }
@@ -83,15 +192,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //What do on when returning to app
     @Override
-    protected void onPostResume() {
+    protected void onPostResume() { //What do on when returning to app
         super.onPostResume();
         receiverWifi = new WifiReceiver(wifiManager, wifiList);
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
         registerReceiver(receiverWifi, intentFilter);
-        //();
+
     }
 
     @Override
@@ -138,30 +246,5 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
         return true;
-
     }
-
 }
-
-
-//    private void getWifi() {
-////        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            //Toast.makeText(MainActivity.this, "version> = marshmallow", Toast.LENGTH_SHORT).show();
-//            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//                Toast.makeText(MainActivity.this, "location permission is off", Toast.LENGTH_SHORT).show();
-//                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_ACCESS_COARSE_LOCATION);
-//            } else if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//                Toast.makeText(MainActivity.this, "fine location permission is off", Toast.LENGTH_SHORT).show();
-//                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_ACCESS_FINE_LOCATION);
-//            } else {
-//                Toast.makeText(MainActivity.this, "All location permissions are on", Toast.LENGTH_SHORT).show();
-//                //don't start scan. button is enough//wifiManager.startScan();
-//                //Toast.makeText(MainActivity.this, "scanning", Toast.LENGTH_SHORT).show();
-//            }
-////        } else { //for older versions
-////            Toast.makeText(MainActivity.this, "All location permissions are on", Toast.LENGTH_SHORT).show();
-////            wifiManager.startScan();
-////            Toast.makeText(MainActivity.this, "scanning2", Toast.LENGTH_SHORT).show();
-////        }
-//    }
-
