770913ef67d269dbc280103a5734e738e6b495e1
ofirasulin3 <ofirasulin3@gmail.com>
2022-01-19 16:24:37 +0200

    adding redundant files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "networisk0418b772": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "S3friendlyName": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
